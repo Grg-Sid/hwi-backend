b2facd04fc6b9e9327de96febccf9cbc4167c04e
Grg-Sid <siddhantgurung007@gmail.com>
2024-08-11 02:27:38 +0530

    feat: added webhooks

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const crypto = require('crypto');
+const { exec } = require('child_process');
+
 const { authenticateToken } = require('./middlewares/auth.middleware');
 const userController = require('./controllers/user.controller');
 const authController = require('./controllers/auth.controller');
@@ -20,6 +23,35 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+app.post('/webhook', (req, res) => {
+    const signature = req.headers['x-hub-signature-256'];
+    const hmac = crypto.createHmac('sha256', WEBHOOK_SECRET);
+    const digest =
+        'sha256=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+
+    if (signature === digest) {
+        if (req.body.ref === 'refs/heads/main') {
+            exec(
+                'cd /path/to/your/app && git pull && npm install && pm2 restart all',
+                (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return res.sendStatus(500);
+                    }
+                    console.log(`stdout: ${stdout}`);
+                    console.error(`stderr: ${stderr}`);
+                    res.sendStatus(200);
+                }
+            );
+        } else {
+            res.sendStatus(200);
+        }
+    } else {
+        res.sendStatus(403);
+    }
+});
 
 app.post('/signup', userController.createUser);
 app.post('/signin', authController.handleLogin);
